3bfa72650fd37fbbcf6cc38a796601146229c775
Electra Chong <electrachong@gmail.com>
2016-02-11 16:44:00 -0800

    Develop to print first two digits

--- dreams_and_dedication_are_a_powerful_combination/0-main.c
+++ dreams_and_dedication_are_a_powerful_combination/0-main.c
@@ -2,9 +2,15 @@
 
 int main(void)
 {
+  print_number(98);
+  print_char('\n');
+  print_number(402);
+  print_char('\n');
   print_number(1024);
   print_char('\n');
   print_number(412);
+  print_char('\n');
+  print_number(835);
   print_char('\n');
   print_number(1);
   print_char('\n');
--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,11 +1,12 @@
 #include "my_functions.h"
-#include <stdio.h>
+#include <stdio.h> /* remove later on, used for testing purposes */
 
 void print_number(int n)
 {
   int a = n;  /* temporarily store n in a variable called a */
   int digits; /* number digits of number n */
   int power;  /* the power of each digit */
+  int digitnumber; /* the single-digit number to print */
 
   /* find the # digits of the number n
      (divide a until it is < 1, and update # digits in each iteration) */
@@ -14,14 +15,34 @@ void print_number(int n)
 	a = a / 10;
       }
 
-    print_char(digits + '0');
-    print_char('\n');
+  printf("digit=%i\n",digits);
 
-    /* find the power of the number n */
+    /* find the power of the number n
+       (for every non-initial digit, multiply power by 10) */
     for (power = 1; digits > 1; digits --)
       {
 	power = power * 10;
       }
 
     printf("power=%i\n",power);
+
+    /* obtain the first digit by dividing n by power */
+    digitnumber = n / power;
+
+    /* print the first digit */ 
+    print_char(digitnumber + '0');
+    print_char('\n');
+      
+    /* subtract power from n to obtain the second digit  */
+    n = n - (digitnumber * power);
+    printf("remainder=%i\n",n);
+    
+    power = power / 10;
+    
+    digitnumber = n / (power);
+    
+    /* print the second digit */
+    print_char(digitnumber + '0');
+    print_char('\n');
+    
 }
