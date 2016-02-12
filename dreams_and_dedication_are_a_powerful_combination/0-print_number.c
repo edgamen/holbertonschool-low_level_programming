94cac24c68cd66853a337a3b79c606ff642b016c
Electra Chong <electrachong@gmail.com>
2016-02-11 20:22:42 -0800

    Store operations in a loop that iterates based on number digits

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -5,8 +5,9 @@ void print_number(int n)
 {
   int a = n;  /* temporarily store n in a variable called a */
   int digits; /* number digits of number n */
+  int digitRetainer; /* a retainer for the digits since the value will be modified */
   int power;  /* the power of each digit */
-  int digitnumber; /* the single-digit number to print */
+  int digitNumber; /* the single-digit number to print */
 
   /* find the # digits of the number n
      (divide a until it is < 1, and update # digits in each iteration) */
@@ -15,7 +16,7 @@ void print_number(int n)
 	a = a / 10;
       }
 
-  printf("digit=%i\n",digits);
+  digitRetainer = digits;
 
     /* find the power of the number n
        (for every non-initial digit, multiply power by 10) */
@@ -24,25 +25,24 @@ void print_number(int n)
 	power = power * 10;
       }
 
-    printf("power=%i\n",power);
+    /* divide number n by power to get the first digit */
+    digitNumber = n / power;
+    
+    /* print the first digit */
+    print_char(digitNumber + '0');
+    
+    /* print the rest of the digits */
+    for (; digitRetainer > 1; digitRetainer--)
+      {
+	/* subtract power times the previous digit from n to obtain the next digit  */
+	n = n - (digitNumber * power);
 
-    /* obtain the first digit by dividing n by power */
-    digitnumber = n / power;
+	power = power / 10;
 
-    /* print the first digit */ 
-    print_char(digitnumber + '0');
-    print_char('\n');
-      
-    /* subtract power from n to obtain the second digit  */
-    n = n - (digitnumber * power);
-    printf("remainder=%i\n",n);
-    
-    power = power / 10;
-    
-    digitnumber = n / (power);
-    
-    /* print the second digit */
-    print_char(digitnumber + '0');
-    print_char('\n');
+	digitNumber = n / (power);
+	
+	/* print the next digit */
+	print_char(digitNumber + '0');
+      }
     
 }
