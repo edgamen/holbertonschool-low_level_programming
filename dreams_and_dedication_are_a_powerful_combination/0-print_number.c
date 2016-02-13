680a2034a81178b817ad6b86b2756618b5fc6b96
Electra Chong <electrachong@gmail.com>
2016-02-13 00:29:01 -0800

    Create separate functions to keep tasks discrete

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -2,39 +2,50 @@
 
 void print_number(int n)
 {
-  int a = n;           /* temporarily store n in a variable called a */
   int digits;          /* number digits of number n */
   int digitRetainer;   /* a retainer for the digits since the value will be modified */
   int power;           /* the power of each digit */
   int output;          /* the single-digit number to print */
 
-  /* find the # digits of the number n by dividing its copy by 10 until it is < 1 */
-  for (digits = 0; a > 0; digits ++)
-      {
-	a = a / 10;
-      }
-
+  digits = get_digits(n);
   digitRetainer = digits;
 
-    /* find the power of the number n (for every non-initial digit, multiply power by 10) */
-    for (power = 1; digits > 1; digits --)
-      {
-	power = power * 10;
-      }
-    
+  power = get_power(digits);    
+
     /* print the rest of the digits */
     for (; digitRetainer > 0; digitRetainer--)
       {
-	/* divide number n by power to get the output for the digit */
 	output = n / (power);
-
-	/* print the output */
 	print_char(output + '0');
-
-	/* subtract power times the previous digit from n to obtain the remaining digits  */
 	n = n - (output * power);
-
 	power = power / 10;
       }
-    
+
+}
+
+int get_digits(int n)
+{
+  int digits;
+
+  /* find the # digits of the number n by dividing its copy by 10 until it is < 1 */
+  for (digits = 0; n > 0; digits ++)
+  {
+    n = n / 10;
+  } 
+
+  return (digits);
+
+}
+
+int get_power(int digits)
+{
+  int power;
+
+  /* find the power of the number n (for every non-initial digit, multiply power by 10) */
+  for (power = 1; digits > 1; digits --)
+  {
+  power = power * 10;
+  }
+
+  return (power);
 }
--- dreams_and_dedication_are_a_powerful_combination/my_functions.h
+++ dreams_and_dedication_are_a_powerful_combination/my_functions.h
@@ -1,2 +1,4 @@
 int print_char(char c);
 void print_number(int n);
+int get_digits(int n);
+int get_power(int digits);
