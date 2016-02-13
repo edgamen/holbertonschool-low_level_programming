8f85d03d656b8a094a60cf4b0a207fb9cb6e54c7
Electra Chong <electrachong@gmail.com>
2016-02-12 21:01:47 -0800

    Remove redundancy and condense code into for loop, rename var to be more accurate

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,48 +1,40 @@
 #include "my_functions.h"
-#include <stdio.h> /* remove later on, used for testing purposes */
 
 void print_number(int n)
 {
-  int a = n;  /* temporarily store n in a variable called a */
-  int digits; /* number digits of number n */
-  int digitRetainer; /* a retainer for the digits since the value will be modified */
-  int power;  /* the power of each digit */
-  int digitNumber; /* the single-digit number to print */
+  int a = n;           /* temporarily store n in a variable called a */
+  int digits;          /* number digits of number n */
+  int digitRetainer;   /* a retainer for the digits since the value will be modified */
+  int power;           /* the power of each digit */
+  int output;          /* the single-digit number to print */
 
-  /* find the # digits of the number n
-     (divide a until it is < 1, and update # digits in each iteration) */
-  for (digits = 0; a >= 1; digits ++)
+  /* find the # digits of the number n by dividing its copy by 10 until it is < 1 */
+  for (digits = 0; a > 0; digits ++)
       {
 	a = a / 10;
       }
 
   digitRetainer = digits;
 
-    /* find the power of the number n
-       (for every non-initial digit, multiply power by 10) */
+    /* find the power of the number n (for every non-initial digit, multiply power by 10) */
     for (power = 1; digits > 1; digits --)
       {
 	power = power * 10;
       }
-
-    /* divide number n by power to get the first digit */
-    digitNumber = n / power;
-    
-    /* print the first digit */
-    print_char(digitNumber + '0');
     
     /* print the rest of the digits */
-    for (; digitRetainer > 1; digitRetainer--)
+    for (; digitRetainer > 0; digitRetainer--)
       {
-	/* subtract power times the previous digit from n to obtain the next digit  */
-	n = n - (digitNumber * power);
+	/* divide number n by power to get the output for the digit */
+	output = n / (power);
+
+	/* print the output */
+	print_char(output + '0');
+
+	/* subtract power times the previous digit from n to obtain the remaining digits  */
+	n = n - (output * power);
 
 	power = power / 10;
-
-	digitNumber = n / (power);
-	
-	/* print the next digit */
-	print_char(digitNumber + '0');
       }
     
 }
