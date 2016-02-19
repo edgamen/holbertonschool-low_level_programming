06aa3bdc84f40df414c776da436a198310c80f80
Electra Chong <electrachong@gmail.com>
2016-02-19 14:44:29 -0800

    Alter loop so initialization sets appropriate conditions to print
    Instead of iterating through unnecessary conditions and using if conditional to instruct when to print

--- keep_calm_and_love_programming/4-print_combination_2.c
+++ keep_calm_and_love_programming/4-print_combination_2.c
@@ -3,20 +3,19 @@
 /* print all possible smallest combinations of two different digits in ascending order */
 void print_combination_2(void)
 {
-  int first_digit = 0;
-  int second_digit = 1;
-
-  printf("%d%d", first_digit, second_digit);
-
-  for (; first_digit < 10; first_digit ++)
+  int first_digit;
+  int second_digit;
+  
+  for (first_digit = 0; first_digit < 10; first_digit ++)
     {
-      for (second_digit = 2; second_digit < 10; second_digit ++)
+      for (second_digit = first_digit + 1; second_digit < 10; second_digit ++)
 	{
-	  if (second_digit > first_digit)   /* print only if the second digit is larger than the first */
+	  printf("%d%d", first_digit, second_digit);
+
+	  if (first_digit < 8 || second_digit < 9)
 	    {
-	      printf(", %d%d", first_digit, second_digit);
+	      printf(", ");
 	    }
 	}
-      second_digit = 0;  /* reset counter for second_digit for iteration of next 10 set of digits */
     }
 }
