8d3656a54c9db77f70f836aa2957e398753933a5
Electra Chong <electrachong@gmail.com>
2016-02-19 14:59:06 -0800

    Add 5-print_number.c

---
+++ keep_calm_and_love_programming/5-main.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include "my_functions.h"
+
+int main(void)
+{
+  print_number(98);
+  print_char('\n');
+  print_number(402);
+  print_char('\n');
+  print_number(1024);
+  print_char('\n');
+  print_number(0);
+  print_char('\n');
+  print_number(-98);
+  print_char('\n');
+  print_number(INT_MAX);
+  print_char('\n');
+  print_number(INT_MIN);
+  print_char('\n');
+  return (0);
+}
--- keep_calm_and_love_programming/my_functions.h
+++ keep_calm_and_love_programming/my_functions.h
@@ -2,5 +2,7 @@ void print_last_digit(int n);
 int get_digits(int n);
 int get_power(void);
 void print_digit(int n);
+void print_digits(int n);
 int print_char(char c);
 void print_combination_2();
+void print_number(int n);
