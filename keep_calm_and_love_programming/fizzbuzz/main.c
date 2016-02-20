e01fb3a21deaccd97262979743870842db037678
Electra Chong <electrachong@gmail.com>
2016-02-19 20:00:07 -0800

    Add fizzbuzz folder

---
+++ keep_calm_and_love_programming/fizzbuzz/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* print numbers from 1 - 100, but replace multiples of 3 with Fizz, multiples of 5 with Buzz, and multiples of both with FizzBuzz */
+int main(void) {
+  int n;
+
+  for (n=1; n <= 100; n++)
+  {
+      if (n % 3 == 0 && n % 5 == 0) {
+	printf("FizzBuzz");
+      }
+      else if (n % 3 == 0) {
+	printf("Fizz");
+      }
+      else if (n % 5 == 0) {
+	printf("Buzz");
+      }
+      else {
+	printf("%d", n);
+      }
+
+      if (n < 100) {
+	printf(" ");
+      }
+  }
+
+  return (0);
+}
