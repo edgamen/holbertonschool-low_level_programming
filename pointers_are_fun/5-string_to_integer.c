3abbea06c42134ce9d0b7990e9c9bbd39394d468
electrachong@gmail.com <electrachong@gmail.com>
2016-03-07 21:59:27 -0800

    Update function to return num instead of printing, and add explanatory comments

--- pointers_are_fun/5-string_to_integer.c
+++ pointers_are_fun/5-string_to_integer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int is_a_number(char c);
 
+/* Convert the first number in a string to an int */
 int string_to_integer(char *s)
 {
   int n, num;
@@ -8,27 +9,28 @@ int string_to_integer(char *s)
 
   for (n = 0, num = 0; s[n] != '\0'; n++)
     {
-      if (s[n] == '-')
+      if (s[n] == '-') /* Keep track of whether or not the number should be negative */
 	{
 	negation_factor *= -1;
 	}
       else if (is_a_number(s[n]))
 	{
-	  num = num * 10 + (s[n] - '0'); 
+	  num = num * 10 + (s[n] - '0'); /* Add each digit in 1st digit, multiplying by 10 to keep proper place */
 
 	 if (is_a_number(s[n + 1]) == 0)
 	    {
-		break;
+	      break;    /* if first number is ending, discontinue for loop */
 	    }
  	}
     }
 
-  num = num * negation_factor;    
-  printf("%d\n", num);
+  /* Multiply number by neg factor in case it should be negative before returning */   
+  num = num * negation_factor; 
  
-  return (0);
+  return (num);
 }
 
+/* Return whether the provided value is a digit char or not */
 int is_a_number(char c) {
   return (c >= '0'&& c <= '9');
 }
