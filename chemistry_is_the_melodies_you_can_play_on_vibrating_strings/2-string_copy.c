6ce5d61f78fa4a00cfaf5891624d84933bffd4cd
Electra Chong <electrachong@gmail.com>
2016-03-16 01:15:45 -0700

    Add third project file, to copy a string

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int find_end(const char *str);
+
+/* Copies a string, including the null-terminating byte */
+char *string_copy(char *dest, const char *src)
+{
+  int dest_index, src_index;
+   
+  for (dest_index = 0, src_index = 0; src_index <= find_end(src) + 1; src_index++, dest_index++)
+    {
+      dest[dest_index] = src[src_index];
+    }
+     
+  return(dest);
+
+}
+
+/* returns index of array that is terminating null byte '\0' */
+int find_end(const char *str)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i++)
+    {
+    }
+
+  return (i);
+}
