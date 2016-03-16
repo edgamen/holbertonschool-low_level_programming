a2c88d5a20e2722fd235528b7d120a10b2b1641e
Electra Chong <electrachong@gmail.com>
2016-03-16 11:08:51 -0700

    Refactor code to eliminate unnecessary if loops and to be more concise. Use for checking condition instead

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -1,41 +1,18 @@
-int find_end(const char *str);
-
 /* Copies a string, including the null-terminating byte */
 char *string_ncopy(char *dest, const char *src, int n)
 {
-  int i;
+  int i = 0;
    
-  if (find_end(src) < n)
+  for (i = 0; i < n && src[i] !='\0'; i++)
     {
-      for (i = 0; src[i] !='\0'; i++)
-	{
-	  dest[i] = src[i];
-	}
-      for (i = find_end(src); i < n; i++)
-	{
-	  dest[i] = '\0';
-	}
+      dest[i] = src[i];
     }
-  else
+  
+  for (; i < n; i++)
     {
-      for (i = 0; i < n; i++)
-	{
-	  dest[i] = src[i];
-	}
+      dest[i] = '\0';
     }
-     
+
   return(dest);
 
-}
-
-/* returns index of array that before terminating null byte '\0' */
-int find_end(const char *str)
-{
-  int i;
-
-  for (i = 0; str[i] != '\0'; i++)
-    {
-    }
-
-  return (i);
 }
