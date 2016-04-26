da9b098250df49176df5966ab6168bdf334ebe75
electrachong@gmail.com <electrachong@gmail.com>
2016-04-26 13:35:37 -0700

    Remove testing for int overflow: can't pass in main anyway

--- cisdoublefun_basics/4-positive_or_not.c
+++ cisdoublefun_basics/4-positive_or_not.c
@@ -1,45 +1,13 @@
 #include "my_functions.h"
 #include <limits.h>
 
-/* check if the argument is greater than INT_MAX */
-int test_for_overflow(int n)
-{
-  if (n > INT_MAX)
-    {
-      return (-1);
-    }
-  else
-    {
-      return (1);
-    }
-}
-
 /* prints P if arg is +, N if -, and Z if it is 0;
    takes an int */
 void positive_or_not(int n)
 {
 
-  /* variable declarations */
-  char s[] = "Error: int overflow";
-  int i;
-
-  /* variable initializations */
-  i = 0;
-  
-  /* test for overflow cases */
-  if (test_for_overflow(n) < 0)
-    {
-      while (s[i] != '\0')
-	{
-	  print_char(s[i]);
-	  i ++;
-	}
-      print_char('\n');
-    }
-  
-  /* in case of no error, print appropriate letter */
-  else {
-    if (n > 0)
+  /* print appropriate letter */
+  if (n > 0)
       {
 	print_char('P');
       }
@@ -51,6 +19,5 @@ void positive_or_not(int n)
       {
 	print_char('Z');
       }
-  }
-  
+   
 }
