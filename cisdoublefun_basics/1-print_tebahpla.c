5a934c1d1fdc7b722e16a3772b01d8111dc18ab2
electrachong@gmail.com <electrachong@gmail.com>
2016-04-26 12:59:26 -0700

    Add a function to print alphabet in reverse order

--- cisdoublefun_basics/1-print_tebahpla.c
+++ cisdoublefun_basics/1-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include "my_functions.h"
 
-/* print the alphabet (in lower-case)*/
-void print_alphabet(void)
+/* print the alphabet in reverse order (lower-case) */
+void print_tebahpla(void)
 {
   /* variable declarations: the starting letter */
   char letter;
 
   /* expressions */ 
-  letter = 'a';
+  letter = 'z';
 
   /* loop through letters of alphabet, printing each */ 
-  while (letter <= 'z')
+  while (letter >= 'a')
     {
       print_char(letter);
-      letter ++;
+      letter --;
     }
 }
