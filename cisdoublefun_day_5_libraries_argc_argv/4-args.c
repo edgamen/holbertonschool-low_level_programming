4d81f05b3005729144ccce1751644634469a8718
Electra Chong <electrachong@gmail.com>
2016-05-01 20:14:41 -0700

    Add a program that prints all arguments starting from the last

---
+++ cisdoublefun_day_5_libraries_argc_argv/4-args.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+void print_string(char *str);
+int print_char(char c);
+
+/* prints all arguments it receives */
+int main(int argc, char **argv)
+{
+  int i;
+
+  i = argc - 1;
+  while (i >= 0)
+    {
+      print_string(argv[i]);
+      i--;
+    }
+
+  return (0);
+}
+
+/* prints a string */
+void print_string(char *str)
+{
+  int i;
+  
+  i = 0;
+  
+  while (str[i] != '\0')
+    {
+      print_char(str[i]);
+      i++;
+    }
+
+  print_char('\n');
+}
+
+/* prints a character */
+int print_char(char c)
+{
+  return (write(1, &c, 1));
+}
