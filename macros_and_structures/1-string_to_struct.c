59299dad0c252cb069420399cd13c1d7f50a5ed1
Electra Chong <electrachong@gmail.com>
2016-05-05 13:50:20 -0700

    Add a function to return pointer to a struct with a string and its length contained

---
+++ macros_and_structures/1-string_to_struct.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+
+int stringlen(char *str);
+char *strcopy(char *dest, char *src);
+
+/* struct String
+{
+  char *str;
+  int length;
+  }; */
+
+/* used to debug string_to_struct function */
+/* int main(void)
+ * {
+ *  char *str = "123456";
+ *  string_to_struct(str);
+ *  return (0);
+ * } 
+*/
+
+
+/* function that returns a pointer to a String struct */
+struct String *string_to_struct(char *str)
+{
+  char *copy_of_string;
+  int string_length;
+  struct String *string_struct = malloc(sizeof(struct String));
+
+  if (string_struct == NULL)
+    {
+      return NULL;
+    }
+
+  /* copy the string given as a parameter into a newly allocated space */
+  string_length = stringlen(str);
+  copy_of_string = malloc(sizeof(char) * string_length);
+  /* if there was not enough memory to create a copy of the string: */
+  if (copy_of_string == NULL)
+    {
+      free(string_struct);
+      return NULL;
+    }
+
+  copy_of_string = strcopy(copy_of_string, str);
+  /* printf("copy of string: %s, address = %p\n", copy_of_string, copy_of_string); */
+
+  string_struct->str = copy_of_string;
+  /* printf("string_struct string: %s, address = %p\n", string_struct->str, string_struct->str); */
+
+  return string_struct;
+}
+
+/* find the length of a given string */
+int stringlen(char *str)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i ++)
+    {
+    }
+
+  return ++i;
+} 
+
+/* copy a source string into a destination string */
+char *strcopy(char *dest, char *src)
+{
+  int i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    {
+      dest[i] = src[i];
+    }
+
+  dest[i] = '\0';
+
+  return dest;
+}
