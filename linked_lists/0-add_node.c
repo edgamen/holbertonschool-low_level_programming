5b5890c86c7bc372604b6fac97834f3d2e6a4711
Electra Chong <electrachong@gmail.com>
2016-05-07 20:01:39 -0700

    Clarify first function with comments to add node at beginning of list

--- linked_lists/0-add_node.c
+++ linked_lists/0-add_node.c
@@ -1,30 +1,49 @@
-#include <Astdlib.h>
+#include <stdlib.h>
 #include "list.h"
 
 char *string_dup(char *str);
 int string_length(char *str);
+List *create_new_node(char *content);
 
-/* allocates a new node and links it ot the list */
+/* allocates a new node and links it to the list -- as the new head */
 int add_node(List **list, char *content)
+{
+  List *new_node;
+
+  new_node = create_new_node(content);
+  if (new_node == NULL)
+    {
+      return (1);
+    }
+
+  /* set the next node of the new to be the next element in the rest of 
+     the list (the previous head / current beginning of the list) */
+  new_node->next = *list;
+  /* now set the beginning of the list to be the new node  */
+  *list = new_node;
+
+  return (0);  
+}
+
+List *create_new_node(char *content)
 {
   List *node;
 
   node = malloc(sizeof(List));
-  if (node == NULL)
-    {
-      return (1);
-    }
+  /* seems redundant so I commented it out:                                                                 
+   * if (node == NULL)                                                                                      
+   * {                                                                                                      
+   *   return NULL;                                                                                         
+   * }                                                                                                      
+   */
   node->str = string_dup(content);
   if (node->str == NULL)
     {
       free(node);
-      return (1);
+      return (NULL);
     }
 
-  node->next = *list;
-  *list = node;
-
-  return (0);  
+  return node;
 }
 
 /* returns a pointer to newly allocated space in memory
