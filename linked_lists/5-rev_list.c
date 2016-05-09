b4302f212c1482eff977c7e20d9fb3b27874d341
Electra Chong <electrachong@gmail.com>
2016-05-08 21:09:56 -0700

    Add a function to reverse the order of nodes in a list

---
+++ linked_lists/5-rev_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "list.h"
+
+/* reverses the order of the nodes in the list */
+void rev_list(List **list)
+{
+  List *current_node;
+  List *next_node;
+  List *previous_node;
+
+  current_node = *list;
+  next_node = current_node->next;
+  current_node->next = NULL;
+  
+  for (previous_node = current_node; next_node != NULL; )
+    {
+      current_node = next_node;
+      next_node = current_node->next;
+      current_node->next = previous_node;
+      previous_node = current_node;
+    }
+
+  /*return current_node;*/
+  *list = current_node;
+}
