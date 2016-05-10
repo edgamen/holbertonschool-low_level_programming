abbde6f82201ebf11034d29f553b66053a2a0181
Electra Chong <electrachong@gmail.com>
2016-05-10 15:13:11 -0700

    Start by checking the number of arguments

--- file_descriptors/3-cat/3-cat.c
+++ file_descriptors/3-cat/3-cat.c
@@ -1,49 +1,15 @@
+#include "my_functions.h"
+
 int main(int ac, char** av)
 {
-
-  /* why do we have to read the standard input? can't we just use ac/av? */
-  /* check there is more than 1 argument (cat): */
-  /* if not, read standard input? */
-  /* read standard input and write it to standard output */
-  /* if standard input is CTL + D then exit/return out of the program */
-  
-  /* for each argument: */
-  {
-    if (/* check if file is a directory */)
-      {
-	/* print "argv[corresponding]: Is a directory\n"; */
-      }
-    else if (/* check if the file exists */)
-      {
-	/* print the file to stdout (use write) */
-      }
-    else /* if the file does not exist (how to check this?) */
-      {
-	/* print "argv[corresponding]: No such file or directory" */
-      }
-  }
+  /* start by checking the number of arguments */
+  if (ac < 2)
+    {
+      /* for now: later we must alter to read stdin most likely */
+      print_string("Please provide a file name.\n");
+      print_string(av[0]);
+      print_char('\n');
+    }
   
   return (0);
 }
-
-
-function to check if file is a directory
-{
-}
-
-function to check if file exists
-{
-}
-
-function to read/write a file to stdout
-{
-  /* we wrote this already, I think */
-}
-
-
-/*
-
-cat [filename]
->>> shows output
-
- */
