f0686badbe5e62515a1af269fedcc376fcbd78ab
Electra Chong <electrachong@gmail.com>
2016-05-13 19:16:47 -0700

    Add functions to print env variables

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -4,7 +4,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <dirent.h>
 #define PROMPT "prompt"
+#define PATH_VAR find_path_var(env)
 #include <stdio.h>
 
 /* A simple UNIX command interpreter (shell) */
@@ -20,10 +22,10 @@ int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, __at
     print_prompt(PROMPT);
     input = read_line(0); 
 
-    /*if (input[0] == NULL)
+    /* if (input[0] == '\n')
       {
-	printf("input is null");
-	} */ /*look into this later */
+	print_string("input is null");
+      } look into this later */
     
     /* if there was some error allocating the string */
     if (input == NULL) {
@@ -40,8 +42,7 @@ int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, __at
     }
 
     /* if empty input is provided by pressing enter */
-    if (exec_argv[0] == NULL)
-      {
+    if (exec_argv[0] == NULL) {
 	free(input);
 	free_str_array(exec_argv);
 	continue;
@@ -55,7 +56,8 @@ int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, __at
       return (0);
     }
     else if (string_compare(exec_argv[0], "env")) {
-      print_string("Need to print env variables.\n");
+      print_array(env);
+      find_exec_path(PATH_VAR, "echo");
     }
     else { 
       /* is there a way to store the value in status 
@@ -78,6 +80,89 @@ int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, __at
   }
   
   return (0);
+}
+
+/* Function: tries to find the path that
+   contains the requested executable */
+char *find_exec_path(char *path_var, __attribute__((unused)) char *exe)
+{
+  char **paths;
+  char *dirname;
+  DIR *dir;
+  int i;
+  
+  if (path_var == NULL) {
+    print_string("Error: could not find $PATH var");
+    return NULL;
+  }
+
+  paths = string_split(path_var, ':');
+  if (paths == NULL) {
+    print_string ("Error: ran out of memory to allocate an array.");
+    return NULL;
+  }
+  
+  /*print_array(paths);*/
+
+  /* for (i = 0; paths[i] != NULL; i++)
+     { */
+      dirname = paths[i];
+      dir = opendir(dirname);
+      if (dir == NULL) {
+	print_string("opendir: ");
+	perror(dirname);
+	/* not sure yet if I want to return
+	   instead of continue through loop:
+	   free_str_array(paths);
+	   return NULL; */
+      }
+      /* search the entries in said dir to see if any are 'echo */
+      search_dir(dir, exe);
+      if (closedir(dir) == -1) {
+	print_string("closedir: ");	
+	perror(dir);
+      }
+      /* } */
+  
+  free_str_array(paths);
+  
+  return NULL;
+}
+
+char *search_dir(DIR *dir, char *exe)
+{
+  struct dirent *dirstruct;
+
+  do {
+    dirstruct = readdir(dir);
+    /* if (dirstruct->name == exe) {
+      printf("%s", dirstruct->name);
+      }*/
+    printf("%s\n", dirstruct->name);
+  } while (dirstruct != NULL);
+
+  return NULL;
+}
+
+
+/* Function: return element in the env
+   array that corresponds to $PATH */
+char *find_path_var(char **env)
+{
+  int i;
+  char *var;
+
+  for (i = 0; env[i] != NULL; i++) {
+    var = env[i];
+    if (var[0] == 'P' && var[1] == 'A' &&
+	var[2] == 'T' && var[3] == 'H'
+	&& var[4] == '=') {
+      /* return the PATH string from after the = */
+      return (var + 5);
+    }
+  }
+
+  return NULL;
 }
 
 /* Function: execute a program passed as an argument 
--- simple_shell/shell_functions.h
+++ simple_shell/shell_functions.h
@@ -4,4 +4,9 @@ void print_prompt(char *s);
 int string_compare(char *s1, char *s2);
 void free_str_array(char **str_array);
 int call_child(char **exec_argv, char **env);
+void print_array(char **array);
+char *find_path_var(char **env);
+char *find_exec_path(char *path_var, __attribute__((unused)) char *exe);
+char *search_dir(DIR *dir, char *exe);
+
 /* int parse_input(char *input, char **exec_argv, char **env, int status); */
