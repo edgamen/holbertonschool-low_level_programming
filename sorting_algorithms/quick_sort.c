d6d83923d83c81821a7217b6e19722e875bef67f
Electra Chong <electrachong@gmail.com>
2016-07-17 19:32:39 -0700

    Revise so list will reliably sort around pivot

--- sorting_algorithms/quick_sort.c
+++ sorting_algorithms/quick_sort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /* Quick sort: Select a random element of the array as pt of comparison (in this case, we pick the first element). 
- * Compare first and last elements; swap if first is larger than last. Otherwise, move selector from last element to preceding. (j - 1)
- * When elements have swapped, move first selector to next element (e.g. i + 1). Compare to last selector (the former first selector).
+ * Move selectors from start and end of arrays until starting selector is greater than pivot and ending selector is less than pivot. Swap them.
+ * Repeat until starting and ending selectors have the same value.
  * If the first and last selector equal each other, split the array.
  * Repeat with recursion, stopping when array is two or less element.
  */
@@ -11,6 +11,7 @@ void quick_sort(int *array, int size)
 {
   int start_index;
   int end_index;
+  int start_value;
   int temp;
 
   if (size < 2) {
@@ -23,18 +24,23 @@ void quick_sort(int *array, int size)
   }
   printf("\n"); */
 
- 
   start_index = 0;
   end_index = size - 1;
+  start_value = array[start_index];
   while (array[start_index] != array[end_index]) {
-    if (array[start_index] > array[end_index]) {
-      temp = array[start_index];
-      array[start_index] = array[end_index];
-      array[end_index] = temp;
+    while (array[start_index] < start_value) {
       start_index++;
-    } else {
+    }
+    while (array[end_index] > start_value) {
       end_index--;
     }
+    if (array[start_index] == array[end_index]) {
+      break;
+    }
+    
+    temp = array[start_index];
+    array[start_index] = array[end_index];
+    array[end_index] = temp;
   }
   /* printf("after: ");
   for (temp = 0; temp < size; temp ++) {
@@ -43,7 +49,7 @@ void quick_sort(int *array, int size)
   printf("\n"); */
 
   if (size > 2) {
-    quick_sort(array, start_index + 1); /* sort array from beginnning to starting index */
+    quick_sort(array, start_index); /* sort array from beginnning to ending value of first partition */
     quick_sort((array + (start_index + 1)), size - (start_index + 1)); /* sort array from after partition to end */
   }
   
