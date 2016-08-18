bb38227f7d4f2e001a3f474999c3e50948db3479
Electra Chong <kompeito@gmail.com>
2016-08-18 13:32:17 -0700

    Add functions to add node to children List

--- n_trees/main.c
+++ n_trees/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "tree.h"
 
+char **string_split(const char *string, char separator);
+
 int ntree_insert(NTree **, char **, char *);
 
 int main(void)
@@ -11,4 +13,6 @@ int main(void)
   tree = NULL;
   ntree_insert(&tree, NULL, "/");
   ntree_insert(&tree, (array = string_split("/", ' ')), "tmp");
+
+  return (0);
 }
--- n_trees/ntree_insert.c
+++ n_trees/ntree_insert.c
@@ -4,8 +4,13 @@
 #include "tree.h"
 
 NTree *new_tree_node(char *data);
+NTree *find_parent_node(NTree *tree, char **parents);
+int add_list_node(List **head, NTree *tree_node);
+List *find_last_list_node(List *head);
 
-/* Insert a node into a N-ary tree */
+/* Insert a node into a N-ary tree
+   Return 0 on success, 1 on failure
+*/
 int ntree_insert(NTree **tree, char **parents, char *data)
 {
   NTree *new_node;
@@ -16,21 +21,43 @@ int ntree_insert(NTree **tree, char **parents, char *data)
     {
       return (1);
     }
-  /* debugging statements: 
-     printf("String in root node: %s\n", (*tree)->str);
-     printf("Addr of children list in root node: %p",	\
-     (void *)((*tree)->children) ); 
-  */
 
   /* Add new_node to root if the tree is empty */
   if (*tree == NULL)
     {
       *tree = new_node;
+
+      /* debugging statements: */
+     printf("String in root node: %s\n", (*tree)->str);
+     printf("Addr of children list in root node: %p\n",	\
+     (void *)((*tree)->children) ); 
+
     }
   else
     {
       parent = find_parent_node(*tree, parents);
+
+      /* Add a List node to the parent's children,
+	 freeing memory and exiting in case of failure */
+      if (add_list_node(&(parent->children), new_node) \
+	  == 1)
+	{
+	  free(new_node->str);
+	  free(new_node);
+	  return (1);
+	}
+
+        /* debugging statements: */
+      printf("Addr of parent tree node: %p\n",	\
+	     (void *)(parent));
+      printf("String in parent tree node: %s\n", parent->str);
+      printf("Addr of children list of parent node: %p\n",	\
+	 (void *)(parent->children));
+      printf("String in first node of children list: %s\n",	\
+	 parent->children->node->str);
+
     }
+
 
   return (0);
 }
@@ -61,18 +88,59 @@ NTree *new_tree_node(char *data)
   return new_node;
 }
 
-/* Find and return the parent node */
-NTree *find_parent_node(Ntree *tree, char **parents)
+/* Add a node to the List; return 0 on success, 1 if error */
+int add_list_node(List **head, NTree *tree_node)
 {
-  Ntree *current_tree_node;
+  List *new_node;
+  List *last_node;
+
+  new_node = malloc(sizeof(List));
+  if (new_node == NULL)
+    {
+      return (1);
+    }
+  new_node->node = tree_node;
+  new_node->next = NULL;
+
+  if (*head == NULL)
+    {
+      *head = new_node;
+    }
+  else
+    {
+      last_node = find_last_list_node(*head);
+      last_node->next = new_node;
+    }
+
+  return (0);
+}
+
+/* Find the last node of a given List */
+List *find_last_list_node(List *head)
+{
+  List *current_node;
+
+  current_node = head;
+  while (current_node->next != NULL)
+    {
+      current_node = current_node->next;
+    }
+
+  return current_node;
+}
+
+/* Find and return the parent node */
+NTree *find_parent_node(NTree *tree, char **parents)
+{
+  NTree *current_tree_node;
   List *current_list;
   int i;
 
   current_tree_node = tree;
   i = 0;
-  while (parents[i] != NULL)
+  while (parents[i + 1] != NULL)
     {
-      printf("Item of array: %s, string in current tree node: %s\n", parents[i], current_tree_node->str)
+      printf("Item of array: %s, string in current tree node: %s\n", parents[i], current_tree_node->str);
       /* If the node string matches the first item in parents,
 	 move onto next item in array and compare to strings
 	 in children of node */
