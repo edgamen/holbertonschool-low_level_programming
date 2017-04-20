13f3bb4ba625b5b3e96d2901a77352d4b2b353d5
Electra Chong <electra.chong@scality.com>
2017-04-19 23:26:26 -0700

    update dummy math

--- maze/src/cast_ray.c
+++ maze/src/cast_ray.c
@@ -3,12 +3,20 @@
 /* Draw the maze */
 int *cast_ray(int column_i)
 {
-    static int coords[3]; /* should be 2, not 3 */
+    static int coords[2];
     /* do mathy things and calculate length of line to draw) */
-    coords[0] = WINDOW_HEIGHT / 4;
-    coords[1] = WINDOW_HEIGHT / 4 * 3;
-    /* do something with column i for now */
-    coords[2] = column_i;
+    if ((column_i >= 0 && column_i <= 40) ||
+        (column_i >= (WINDOW_WIDTH - 40) && column_i <= (WINDOW_WIDTH)))
+    {
+        coords[0] = 0;
+        coords[1] = 0;
+    }
+    else
+    {
+        coords[0] = WINDOW_HEIGHT / 4;
+        coords[1] = WINDOW_HEIGHT / 4 * 3;
+    }
+
 
     return coords;
 }
